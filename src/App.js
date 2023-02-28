962fce4e2e554acabd8ab271f1dbc70e5e20a1ab
Abhishek Yadav <abhiyd2401@gmail.com>
2023-02-28 21:37:38 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Sidebar from "./Sidebar";
-import Main from "./Main";
+import Main from "./pages/Main";
+import Posts from "./pages/Post"
+import Apps from "./pages/Apps";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="w-full flex bg-gray-50">
       <Sidebar/>
-      <Main />
+      
+      <div className="flex flex-col">
+       <Main />
+       <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/apps" element={<Apps/>} />
+      </Routes>
+      </div>
+      
     </div>
   );
 }
