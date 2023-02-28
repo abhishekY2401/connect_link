2dcfd03f25bb1f6dde42d6f23f4164f182873526
Abhishek Yadav <abhiyd2401@gmail.com>
2023-02-28 21:40:19 +0530

    added gap in div

---
+++ src/pages/Post.js
@@ -0,0 +1,19 @@
+import Card from "../components/Card";
+import People from "../assets/people_working.avif";
+
+const Post = () => {
+  return (
+    <div className="mt-8 p-5">
+      <div className="font-sans text-xl">Recommended for you</div>
+      <div className="mt-4 flex justify-center items-center gap-6">
+        <Card image={People} title="Lorem ipsum dolor sit amet" description="How do you create compelling presentations that wow your colleagues and impress your managers?" link="https://google.com/" />
+        <Card image={People} title="Lorem ipsum dolor sit amet" description="How do you create compelling presentations that wow your colleagues and impress your managers?" link="https://google.com/" />
+        <Card image={People} title="Lorem ipsum dolor sit amet" description="How do you create compelling presentations that wow your colleagues and impress your managers?" link="https://google.com/" />
+        <Card image={People} title="Lorem ipsum dolor sit amet" description="How do you create compelling presentations that wow your colleagues and impress your managers?" link="https://google.com/" />
+
+      </div>
+    </div>
+  )
+}
+
+export default Post
